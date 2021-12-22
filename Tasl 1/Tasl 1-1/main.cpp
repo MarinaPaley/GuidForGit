758cb943c73bb8b84d4f0a735d7e535d503e409a
Marina Paley <marina_paley@mail.ru>
2021-12-22 15:11:29 +0300

    Исправления согласно замечаниям #3

--- Tasl 1/Tasl 1-1/main.cpp
+++ Tasl 1/Tasl 1-1/main.cpp
@@ -4,16 +4,16 @@
 
 /**
 *  \brief Функция расчета по заданной формуле.
-*  \param x Аргумент функции.
-*  \param y Аргумент функции.
+*  \param x Первый параметр функции.
+*  \param y Второй параметр функции.
 *  \return Значение функции.
 */
 double getA(double x, double y);
 
 /**
 *  \brief Функция расчета по заданной формуле.
-*  \param x Аргумент функции.
-*  \param z Аргумент функции.
+*  \param x Первый параметр функции.
+*  \param z Второй параметр функции.
 *  \return Значение функции.
 */
 double getB(double x, double z);
@@ -23,22 +23,24 @@ double getB(double x, double z);
 */
 int main()
 {
-	const double x = -1.3;
-	const double y = 7.6;
-	const double z = -1.85;
-	const double a = getA(x, y);
-	const double b = getB(x, z);
+	const auto x = -1.3;
+	const auto y = 7.6;
+	const auto z = -1.85;
+	const auto a = getA(x, y);
+	const auto b = getB(x, z);
+
 	std::cout << "x = " << x << " y = " << y << " z = " << z << "\n";
 	std::cout << "a = " << a << " b = " << b << std::endl;
+
 	return 0;
 }
 
 double getA(const double x, const double y)
 {
-	return exp(x / y) - cbrt(y / x);
+	return std::exp(x / y) - std::cbrt(y / x);
 }
 
 double getB(const double x, const double z)
 {
-	return 2 * cos(x - M_PI_2) / (1.0/ 2 + sin(z) * sin(z));
+	return 2 * std::cos(x - M_PI_2) / (1.0 / 2 + std::sin(z) * std::sin(z));
 }
